8c8f7253842e063a7884e58ea32f26aeffb7ec2f
Ross Basarevych <basarevych@gmail.com>
2017-03-27 10:43:02 +0300

    New vars.json

--- servers/directory.js
+++ servers/directory.js
@@ -241,19 +241,190 @@ class Directory extends EventEmitter {
                 return this._filer.createDirectory(
                     this.rootDir,
                     { mode: this.dirMode, uid: this.user, gid: this.group }
-                )
+                );
             })
             .then(() => {
                 return this._filer.createDirectory(
                     this.dataDir,
                     { mode: this.dirMode, uid: this.user, gid: this.group }
-                )
+                );
             })
             .then(() => {
                 return this._filer.createDirectory(
                     this.stateDir,
                     { mode: this.dirMode, uid: this.user, gid: this.group }
-                )
+                );
+            })
+            .then(() => {
+                this._logger.debug('directory', 'Starting the server');
+
+                let exists;
+                try {
+                    fs.accessSync(path.join(this.dataDir, '.bhdir.json'), fs.constants.F_OK);
+                    exists = true;
+                } catch (error) {
+                    exists = false;
+                }
+
+                return new Promise((resolve, reject) => {
+                        let tries = 0;
+                        let retry = () => {
+                            if (++tries > 3)
+                                return reject(new Error('.bhdir.json is damaged'));
+
+                            let upgrading = false;
+                            this._filer.lockUpdate(
+                                    path.join(this.dataDir, '.bhdir.json'),
+                                    contents => {
+                                        try {
+                                            this.bhdirInfo = JSON.parse(contents);
+                                            if (typeof this.bhdirInfo !== 'object')
+                                                return Promise.reject(new Error('.bhdir.json is damaged'));
+                                        } catch (error) {
+                                            if (exists) {
+                                                setTimeout(() => { retry(); }, 1000);
+                                                return Promise.resolve(contents);
+                                            }
+
+                                            this.bhdirInfo = {};
+                                        }
+
+                                        if (!this.bhdirInfo.directory)
+                                            this.bhdirInfo.directory = {};
+                                        if (!this.bhdirInfo.directory.format)
+                                            this.bhdirInfo.directory.format = 1;
+                                        if (!this.bhdirInfo.directory.upgrading)
+                                            this.bhdirInfo.directory.upgrading = false;
+
+                                        if (this.bhdirInfo.directory.format !== 2) {
+                                            upgrading = true;
+                                            this.bhdirInfo.directory.format = 2;
+                                            this.bhdirInfo.directory.upgrading = this._state.sessionId;
+                                        }
+
+                                        return Promise.resolve(JSON.stringify(this.bhdirInfo, undefined, 4) + '\n');
+                                    },
+                                    { mode: this.fileMode, uid: this.user, gid: this.group }
+                                )
+                                .then(() => {
+                                    resolve(upgrading);
+                                })
+                                .catch(error => {
+                                    reject(error);
+                                });
+                        };
+                        retry();
+                    })
+                    .then(upgrading => {
+                        if (!upgrading)
+                            return;
+
+                        let upgradeDir = dir => {
+                            return new Promise((resolve, reject) => {
+                                try {
+                                    let stats = fs.statSync(dir);
+                                    if (!stats.isDirectory())
+                                        return resolve();
+                                } catch (error) {
+                                    return reject(error);
+                                }
+
+                                let processor;
+                                try {
+                                    let stats = fs.statSync(path.join(dir, '.vars.json'));
+                                    if (stats.isFile()) {
+                                        processor = this._filer.lockUpdate(
+                                            path.join(dir, '.vars.json'),
+                                            contents => {
+                                                let json;
+                                                try {
+                                                    json = JSON.parse(contents);
+                                                    if (typeof json !== 'object')
+                                                        return Promise.resolve(contents);
+                                                } catch (error) {
+                                                    return Promise.resolve(contents);
+                                                }
+
+                                                let result = {};
+                                                for (let key of Object.keys(json)) {
+                                                    result[key] = {
+                                                        id: uuid.v4(),
+                                                        ctime: Math.round(Date.now() / 1000),
+                                                        mtime: Math.round(Date.now() / 1000),
+                                                        value: json[key],
+                                                    };
+                                                }
+
+                                                return Promise.resolve(JSON.stringify(result, undefined, 4) + '\n');
+                                            }
+                                        );
+                                    }
+                                } catch (error) {
+                                    // do nothing
+                                }
+
+                                Promise.resolve()
+                                    .then(() => {
+                                        if (processor)
+                                            return processor;
+                                    })
+                                    .then(() => {
+                                        let promises = [];
+                                        let files = fs.readdirSync(dir);
+                                        for (let file of files)
+                                            promises.push(upgradeDir(path.join(dir, file)));
+
+                                        if (promises.length)
+                                            return Promise.all(promises);
+                                    })
+                                    .then(() => {
+                                        resolve();
+                                    })
+                                    .catch(error => {
+                                        reject(error);
+                                    })
+                            });
+                        };
+
+                        return new Promise(resolve => {
+                                setTimeout(resolve, 10 * 1000);
+                            })
+                            .then(() => {
+                                return upgradeDir(this.dataDir);
+                            })
+                            .then(() => {
+                                return this._filer.lockUpdate(
+                                    path.join(this.dataDir, '.bhdir.json'),
+                                    contents => {
+                                        let json;
+                                        try {
+                                            json = JSON.parse(contents);
+                                            if (typeof json !== 'object')
+                                                return Promise.resolve(contents);
+                                        } catch (error) {
+                                            return Promise.resolve(contents);
+                                        }
+
+                                        if (json.directory)
+                                            json.directory.upgrading = false;
+
+                                        return Promise.resolve(JSON.stringify(json, undefined, 4) + '\n');
+                                    }
+                                );
+                            });
+                    })
+                    .then(() => {
+                        return this._filer.lockRead(path.join(this.dataDir, '.bhdir.json'))
+                            .then(contents => {
+                                let json = JSON.parse(contents);
+                                if (json.directory.upgrading && json.directory.upgrading !== this._state.sessionId) {
+                                    return this._app.info(
+                                        'Directory is being upgraded by other daemon - restarting...\n',
+                                        () => { process.exit(200); }
+                                    );
+                                }
+                            });
+                    });
             });
     }
 
@@ -878,14 +1049,14 @@ class Directory extends EventEmitter {
     }
 
     /**
-     * Retrieve watcher server
-     * @return {Watcher}
+     * Retrieve state server
+     * @return {State}
      */
-    get _watcher() {
-        if (this._watcher_instance)
-            return this._watcher_instance;
-        this._watcher_instance = this._app.get('servers').get('watcher');
-        return this._watcher_instance;
+    get _state() {
+        if (this._state_instance)
+            return this._state_instance;
+        this._state_instance = this._app.get('servers').get('state');
+        return this._state_instance;
     }
 }
 
